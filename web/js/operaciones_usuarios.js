4220f6d9977a969b368e10e00cdfa2a311f73126
Osnayder <Osnayder@PC-OSNAYDER>
2021-06-04 22:05:17 -0500

    CRUD completo de clientes

--- web/js/operaciones_usuarios.js
+++ web/js/operaciones_usuarios.js
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 
+var usuario_u;
+
 window.onload = function(){ pullUsuarios();};
 
 document.getElementById('actualizar_tabla_usuarios').addEventListener('click',pullUsuarios,false);
@@ -42,6 +44,7 @@ function desplegar_datos_usuarios(listajson){
         accion_2.setAttribute("class", "btn btn-secondary btn-sm");
         
         accion_1.addEventListener("click",ventana_edit_cliente,false);
+        accion_2.addEventListener("click",ventana_delete_cliente,false);
         
         column_1.innerHTML = listajson[i].ID_usuario;
         column_2.innerHTML = listajson[i].nombres;
@@ -84,15 +87,95 @@ function pullUsuarios(){
     console.log("Se envio la solicitud");
 }
 
+function eliminarUsuarios(identificacion){   
+    var http = new XMLHttpRequest();
+    
+    http.onreadystatechange = function() {
+        if(http.readyState == 4) {  // Cuando readyState es igual a 4 la respuesta esta lista
+            if(http.status == 200){ // Cuando el status es 200 la respuesta llega sin problemas
+                pullUsuarios();
+                console.log("Se elimino un usuario");
+            }else{
+                console.log("Error al eliminar");
+            }
+        }
+    };
+    
+    http.open("POST","/APIRESTfull/resources/services/capstore/eliminarusuarios", true);
+    http.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
+    http.send(identificacion);
+    console.log("Se envio la solicitud paraa eliminar");
+}
+
+function editarUsuarios(id_u,id_r,nombre,apellido,fecha_n,genero,password){   
+    var http = new XMLHttpRequest();
+    
+    http.onreadystatechange = function() {
+        if(http.readyState == 4) {  // Cuando readyState es igual a 4 la respuesta esta lista
+            if(http.status == 200){ // Cuando el status es 200 la respuesta llega sin problemas
+                document.getElementById("modal_c_edit").style.display='none';
+                pullUsuarios();
+                console.log("Se actualizo el usuario");
+            }else{
+                console.log("Error al actaulizar el usuario");
+            }
+        }
+    };
+    http.open("POST","/APIRESTfull/resources/services/capstore/actualizarusuario", true);
+    http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    http.send(JSON.stringify({"ID_usuario":id_u,"ID_rol":id_r,"password":password,"nombres":nombre,"apellidos":apellido,"fecha_nacimiento":fecha_n,"genero":genero}));
+    console.log("Se envio la solicitud para editar");
+}
+
 function ventana_edit_cliente(Event){
-	document.getElementById("modal_c_edit").style.display = 'block';
-	document.getElementById('cerrar_c_edit').addEventListener("click",function(event){
-		document.getElementById("modal_c_edit").style.display='none';
-	},false);
-	
-	document.getElementById("edit_c_nombre").value = Event.target.parentNode.parentNode.getElementsByClassName("nombres")[0].innerText;
-        document.getElementById("edit_c_apellido").value = Event.target.parentNode.parentNode.getElementsByClassName("apellidos")[0].innerText;
-        document.getElementById("edit_c_fecha").value = Event.target.parentNode.parentNode.getElementsByClassName("fecha_nacimiento")[0].innerText;
-        document.getElementById("edit_c_sexo").value = Event.target.parentNode.parentNode.getElementsByClassName("sexo")[0].innerText;
-	
+    document.getElementById("modal_c_edit").style.display = 'block';
+    document.getElementById('cerrar_c_edit').addEventListener("click",function(){
+            document.getElementById("modal_c_edit").style.display='none';
+    },false);
+
+    var http = new XMLHttpRequest();
+    
+    http.onreadystatechange = function() {
+        if(http.readyState == 4) {  
+            if(http.status == 200){ 
+                var usuario = JSON.parse(http.responseText);
+                usuario_u = usuario;
+                document.getElementById("edit_c_nombre").value = usuario.nombres;
+                document.getElementById("edit_c_apellido").value = usuario.apellidos;
+                document.getElementById("edit_c_fecha").value = usuario.fecha_nacimiento;
+                document.getElementById("edit_c_sexo").value = usuario.genero;
+                document.getElementById("edit_c_contrasena_a").value = usuario.password;
+                console.log("Se resivio el usuario para editar");
+            }else{
+                console.log("Error al buscar el usuario para editar");
+            }
+        }
+    };
+    
+    http.open("GET","/APIRESTfull/resources/services/capstore/buscarUsuario?id="+Event.target.parentNode.parentNode.getElementsByClassName("cc")[0].innerText, true);
+    http.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
+    http.send();
+    console.log("Se envio la solicitud para editar");
+}
+
+document.getElementById("btn_guardar_cliente_edit").addEventListener('click',function(){
+    var nombre_out = document.getElementById("edit_c_nombre").value;
+    var apellido_out = document.getElementById("edit_c_apellido").value;
+    var fecha_nacimiento_out = document.getElementById("edit_c_fecha").value;
+    var genero_out = document.getElementById("edit_c_sexo").value;
+    var password = document.getElementById("edit_c_contrasena_a").value;
+    editarUsuarios(usuario_u.ID_usuario,usuario_u.ID_rol,nombre_out,apellido_out,fecha_nacimiento_out,genero_out,password);
+},false);
+
+function ventana_delete_cliente(Event){
+    document.getElementById('modal_c_delete').style.display = 'block';
+    
+    document.getElementById('btn1_modal_c_delete').addEventListener('click',function(){
+        document.getElementById("modal_c_delete").style.display='none';
+        eliminarUsuarios(Event.target.parentNode.parentNode.getElementsByClassName("cc")[0].innerText);
+    },false);
+    
+    document.getElementById('btn2_modal_c_delete').addEventListener('click',function(){
+        document.getElementById("modal_c_delete").style.display='none';
+    },false);
 }
